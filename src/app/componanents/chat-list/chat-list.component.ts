c081157dc0feaea916a2b90e5701e7fdb514eb4c
massine <massineharcherras@gmail.com>
2025-01-15 00:49:40 +0200

    tous ca marche construction en cours 3 56 17

---
+++ src/app/componanents/chat-list/chat-list.component.ts
@@ -0,0 +1,20 @@
+import {Component, input, InputSignal} from '@angular/core';
+import {ChatResponse} from '../../services/models/chat-response';
+import {ChatService} from '../../services/services/chat.service';
+
+@Component({
+  selector: 'app-chat-list',
+  imports: [],
+  templateUrl: './chat-list.component.html',
+  styleUrl: './chat-list.component.scss'
+})
+export class ChatListComponent {
+  chats: InputSignal<ChatResponse[]> = input<ChatResponse[]>([]);
+  searchNewContact=false
+
+
+
+  searchContact() {
+
+  }
+}
---
+++ src/app/pages/main/main.component.ts
@@ -0,0 +1,38 @@
+import {Component, OnInit} from '@angular/core';
+import {ChatListComponent} from '../../componanents/chat-list/chat-list.component';
+import {ChatResponse} from '../../services/models/chat-response';
+import {ChatService} from '../../services/services/chat.service';
+
+@Component({
+  selector: 'app-main',
+  imports: [
+    ChatListComponent
+  ],
+  templateUrl: './main.component.html',
+  styleUrl: './main.component.scss'
+})
+export class MainComponent implements OnInit {
+
+  chats: Array<ChatResponse>=[];
+
+  constructor(private chatService: ChatService) {
+
+  }
+
+  ngOnInit(): void {
+    this.getAllChats();
+  }
+
+  private getAllChats() {
+    this.chatService.getChatsByReceiver()
+      .subscribe({
+        next: (res) => {
+          this.chats = res;
+        }
+      });
+  }
+
+
+
+
+}
